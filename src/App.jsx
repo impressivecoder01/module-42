d4e6b3fbde9f50fdeaf54e9d680f8efbbfe422c6
impressivecoder01 <rohanrahman251@gmail.com>
2024-10-23 21:08:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     
+      
+      <h1 className='text-6xl bg-red-300'>Knowledge Cafe</h1>
+      <h2>Knowledge</h2>
+    </>
+  )
+}
+
+export default App
