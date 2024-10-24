d77ce159f49a4d7b93852ad3253f5fa495cd519f
impressivecoder01 <rohanrahman251@gmail.com>
2024-10-24 20:42:11 +0600

    blog section added

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,28 @@
 
 const Blog = ({blog}) => {
     console.log(blog)
+    const {title, cover_img, reading_time, author, author_img, posted_date, hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover_img} alt="" />
+            <div className="flex justify-between">
+                <div className="flex gap-2">
+                    <img className="w-14" src={author_img} alt="" />
+                    <div>
+                        <h4 className="text-2xl">{author}</h4>
+                        <p>{posted_date}</p>        
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map(hash => <span><a href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
